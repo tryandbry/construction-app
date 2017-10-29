cd533b55b4a33c69c04501f6995c2b6e38ad42d5
bryan <bryan@localhost.localdomain>
2017-10-29 14:03:17 -0500

    adding Bootstrap 4.0.0-beta.2

--- server/index.js
+++ server/index.js
@@ -4,6 +4,10 @@ const path = require('path');
 const PORT = process.env.PORT || 3000;
 
 app.use(express.static(path.resolve(__dirname,'../dist')));
+app.use(express.static(path.resolve(__dirname,'../node_modules/bootstrap/dist/css')));
+app.use(express.static(path.resolve(__dirname,'../node_modules/bootstrap/dist/js')));
+app.use(express.static(path.resolve(__dirname,'../node_modules/jquery/dist')));
+app.use(express.static(path.resolve(__dirname,'../node_modules/popper.js/dist/umd')));
 
 app.get('/*', function(request, response) {
   response.sendFile(path.resolve(__dirname, '../dist', 'index.html'));
