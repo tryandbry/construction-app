c0e8d2fa143768abd0b49d7d9624daf139c86e10
bryan <bryan@localhost.localdomain>
2017-11-08 01:13:27 -0600

    resets demolition table

--- app/views/estimate/demolition.js
+++ app/views/estimate/demolition.js
@@ -26,40 +26,40 @@ class demolition extends React.Component {
     const tasks = Object.keys(this.props.tasks);
 
     return (
-      <div id="estimateDemolition" className="row">
-        <div className="col-12">
-          <h2>Demolition</h2>
-          <h3>Enter estimate</h3>
-        </div>
-        <div className="col-12">
-          <table className="table">
-            <thead>
-              <tr>
-                <th>Item</th>
-                <th>Description of Work</th>
-                <th>Quantity</th>
-                <th>Unit</th>
-                <th>Unit Price</th>
-                <th>Cost</th>
-              </tr>
-            </thead>
-            <tbody>
-              {tasks.map( (task,i) => 
-              <tr key={task}>
-                <td>{i}</td>
-                <td>{task}</td>
-                <td><DemoInput
-                  task={task}
-                  value={this.state[task].qty}
-                  handleChange={this.handleChange}
-                /></td>
-                <td>{this.state[task].unit}</td>
-                <td>{printPrice(this.state[task].unitPrice)}</td>
-                <td>{printPrice(this.state[task].cost)}</td>
-              </tr>
-              )}
-            </tbody>
-          </table>
+      <div id="estimateDemolition">
+        <div className="row">
+          <div className="col-12">
+            <h2>Demolition</h2>
+            <h3>Enter estimate</h3>
+            <table className="table">
+              <thead>
+                <tr>
+                  <th>Item</th>
+                  <th>Description of Work</th>
+                  <th>Quantity</th>
+                  <th>Unit</th>
+                  <th>Unit Price</th>
+                  <th>Cost</th>
+                </tr>
+              </thead>
+              <tbody>
+                {tasks.map( (task,i) => 
+                <tr key={task}>
+                  <td>{i}</td>
+                  <td>{task}</td>
+                  <td><DemoInput
+                    task={task}
+                    value={this.state[task].qty}
+                    handleChange={this.handleChange}
+                  /></td>
+                  <td>{this.state[task].unit}</td>
+                  <td>{printPrice(this.state[task].unitPrice)}</td>
+                  <td>{printPrice(this.state[task].cost)}</td>
+                </tr>
+                )}
+              </tbody>
+            </table>
+          </div>
         </div>
       </div>
     );
