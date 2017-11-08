1f711da833af4a07105d48ff16e89abadad966d3
bryan <bryan@localhost.localdomain>
2017-11-08 00:18:48 -0600

    re-factored sidebar

--- app/views/estimate/sidebar.js
+++ app/views/estimate/sidebar.js
@@ -1,42 +1,26 @@
 import React from 'react';
 import { connect } from 'react-redux';
 
-//import { selectTrades } from '../../store/estimate';
+const sidebar = (props) =>
+  <div id='sidebar'>
+    <table className="table">
+      <thead>
+        <tr>
+          <th>Selected Trades</th>
+        </tr>
+      </thead>
+      <tbody>
+        {props.selectedTrades.map( (trade) =>
+        <tr key={'selected'+trade}>
+          <td>{trade}</td>
+        </tr>
+        )}
+      </tbody>
+    </table>
+  </div>
 
-class sidebar extends React.Component {
-  constructor(props){
-    super();
-    this.state = {
-      selectedTrades: props.selectedTrades,
-    };
-  }
-
-  render(){
-
-    return (
-      <div id='sidebar'>
-        <table className="table">
-          <thead>
-            <tr>
-              <th>Selected Trades</th>
-            </tr>
-          </thead>
-          <tbody>
-            {this.state.selectedTrades.map( (trade) =>
-            <tr key={'selected'+trade}>
-              <td>{trade}</td>
-            </tr>
-            )}
-          </tbody>
-        </table>
-      </div>
-    );
-  }
-}
-
-const mapState = (state) => ({
-  //trades: state.estimate.trades,
-  selectedTrades: state.estimate.selectedTrades,
+const mapState = ({estimate}) => ({
+  selectedTrades: estimate.selectedTrades,
 });
 
 export default connect(mapState)(sidebar);
