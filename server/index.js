721563a92811121a1dc54dc01c62d7119d0aea83
Bryan <bryan@greybox.attlocal.net>
2017-11-23 11:10:01 -0600

    sets express app.listen to be dependent on db connection

--- server/index.js
+++ server/index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 const path = require('path');
 const PORT = process.env.PORT || 3000;
+const db = require('./db');
 
 app.use(express.static(path.resolve(__dirname,'../dist')));
 app.use(express.static(path.resolve(__dirname,'../node_modules/bootstrap/dist/css')));
@@ -13,7 +14,9 @@ app.get('/*', function(request, response) {
   response.sendFile(path.resolve(__dirname, '../dist', 'index.html'));
 });
 
-
-app.listen(PORT,function(){
-  console.log(`listening on port ${PORT}`);
+db.on('error', console.error.bind(console, 'db connection error'));
+db.on('open',function(){
+  app.listen(PORT,function(){
+    console.log(`listening on port ${PORT}`);
+  });
 });
