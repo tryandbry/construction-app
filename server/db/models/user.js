4cb133830901f34074db162c8e549f3a0d76def7
Bryan <bryan@greybox.attlocal.net>
2017-11-23 12:23:41 -0600

    user model file exports a Model [vs. Schema]

--- server/db/models/user.js
+++ server/db/models/user.js
@@ -1,9 +1,11 @@
-var Schema = require('mongoose').Schema;
+const mongoose = require('mongoose');
 
-module.exports = new Schema({
+const User = mongoose.Schema({
   name: String,
   username: { type: String, required: true, unique: true },
   password: { type: String, required: true },
   created_at: Date,
   updated_at: Date,
 });
+
+module.exports = mongoose.model('User',User);
